0830211aabdf8153c737ab02c34971ac313ea193
Andrew Nagle <annagle@tcd.ie>
2017-11-08 14:30:04 +0000

    Everthing complete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,38 @@
 class ReviewsController < ApplicationController
+
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
 end
+
+  #   assigns a new review as @review (FAILED - 9)
+  # POST create
+  #   with valid params
+  #     creates a new Review (FAILED - 10)
+  #     assigns a newly created review as @review (FAILED - 11)
+  #     redirects to the parent restaurant (FAILED - 12)
+  #   with invalid params
+  #     assigns a newly created but unsaved review as @review (FAILED - 13)
+  #     re-renders the 'new' template (FAILED - 14)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: "0799282982",
+    category:        "italian"
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: "079930482982",
+    category:        "italian"
+  },
+    {
+    name:         'Flat Iron',
+    address:      '77 Curtain Rd, London EC2A 3BS',
+    phone_number: "07993048292232",
+    category:        "french"
+  },
+    {
+    name:         'Le Bab',
+    address:      'Top Floor, Kingly Court, Carnaby St, Carnaby, London W1B 5PW',
+    phone_number: "02074399222",
+    category:        "french"
+  },
+    {
+    name:         'Relais de Venise',
+    address:      '5 Throgmorton St, London EC2N 2AD',
+    phone_number: "02076386325",
+    category:        "french"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
